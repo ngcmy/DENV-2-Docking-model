718f8fb3eff66712163c2b8d26f320b3e7b67a58
ngcmy <nguyenngocmy.2873@gmail.com>
2025-12-31 11:35:14 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# .streamlit/setup.sh
+chmod +x ./vina/vina_1.2.7_linux_x86_64
+chmod +x ./utils/ligand_preprocessing/mk_prepare_ligand.py
+chmod +x ./utils/ligand_preprocessing/scrub.py
